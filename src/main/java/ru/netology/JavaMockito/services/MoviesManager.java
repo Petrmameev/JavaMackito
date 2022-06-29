1838dd7a0f2303b174301ecc03bbfa1430030f07
Petr <petr_mameev@mail.ru>
2022-06-29 22:41:53 +0700

    third commit

--- src/main/java/ru/netology/JavaMockito/services/MoviesManager.java
+++ src/main/java/ru/netology/JavaMockito/services/MoviesManager.java
@@ -1,35 +1,36 @@
-package ru.netology.JavaMockito
+package ru.netology.JavaMockito.services;
+
 public class MoviesManager {
     private String[] movies = new String[0];
     private int limit;
-    public MoviesManager(){
+
+    public MoviesManager() {
         limit = 10;
-
     }
-    public MoviesManager (int limit){
+
+    public MoviesManager(int limit) {
         this.limit = limit;
-
     }
+
     public void add(String movie) {
         String[] tmp = new String[movies.length + 1];
         for (int i = 0; i < movies.length; i++) {
-            tmp [i] = movies[i];
+            tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = movie;
         movies = tmp;
 
     }
 
-    public String[] findAll(){
+    public String[] findAll() {
         return movies;
     }
 
-    public String[] findLast(int limit) {
+    public String[] findLast() {
         int resultLength;
         if (movies.length < limit) {
             resultLength = movies.length;
-        }
-        else {
+        } else {
             resultLength = limit;
         }
         String[] tmp = new String[resultLength];
